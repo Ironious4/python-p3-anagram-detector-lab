b4ad3aa03191b315eeaa631b72e2554edc6f7567
Ironious4 <tyronekariuki@gmail.com>
2024-09-02 18:20:07 +0300

    Add lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
-# your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word=word.lower()
+
+    def match(self, possible_anagrams):
+        sorted_word= sorted(self.word)
+        return[w for w in possible_anagrams if sorted(w.lower())==sorted_word]
+        
